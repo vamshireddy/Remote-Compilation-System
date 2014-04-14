96d58e71bad9cd66c146a9fd3654b8f58b473987
vamshireddy <k.vamshi2008@gmail.com>
2014-04-14 11:48:42 +0530

    Added details for compiling on remote hosts, Not totally working

--- client.c
+++ client.c
@@ -1,6 +1,8 @@
 #include<netinet/in.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<sys/types.h>
+#include<fcntl.h>
 #define MAX_LENGTH 15
 #define MAX_COMMANDS 10
 
@@ -31,11 +33,12 @@ int main()
 	connect(sock,(struct sockaddr*)&server,sizeof(server));
 
 	/* Tell, what service you want to server */
-	char c = '0';
-	printf("Enter '0' for Command execution, '1' for Program execution \n");
+	char c[4];
+	printf("Enter '0000' for Command execution, '1111' for Program execution \n");
+	fflush(stdin);
+	scanf("%s",c);
 	write(sock,&c,1);
-	
-	if( c == '0' )
+	if( c[0] == '0' )
 	{
 		struct command cmd;
 		printf("Enter the command to be executed : ");
@@ -57,16 +60,39 @@ int main()
 		char* byte_pointer = (char*)&cmd;
 		write(sock,byte_pointer,sizeof(struct command));
 		/* Expecting the output */
-		printf("Done writing!\n");
-		char c;
-		while( read(sock,&c,1)!=0 )
-		{
-			printf("%c",c);
-		}
-		printf("\nDONE \n");
+		printf("The command has been sent succesfully \nWaiting for the result\n");		
 	}
 	else
 	{
 		// Send the executable file
+		printf("Enter the file to be sent for execution : ");
+		char file_name[MAX_LENGTH];
+		scanf("%s",file_name);
+		/* Use the file name and open the file for reading */
+		int file = open(file_name,O_RDONLY,0777);
+		if( file < 0 )
+		{
+			perror(" ");
+			exit(0);
+		}
+		char c;
+		while( read(file,&c,1) != 0 )
+		{
+			if( c == EOF )
+			{
+				write(sock,&c,1);
+				break;
+			}
+			write(sock,&c,1);
+		}
+		c = EOF;
+		write(sock,&c,1);
+		printf("The file has been sent succesfully \nWaiting for the result\n");			
 	}
+	char ch;
+	while( read(sock,&ch,1)!=0 )
+	{
+		printf("%c",ch);
+	}
+	printf("\nDONE \n");
 }
